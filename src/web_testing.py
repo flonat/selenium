eec98796af660e7dd3b4a2033ac14266f30dfaeb
danielpartida <daniel.partida@d-fine.de>
2021-12-14 18:56:54 +0100

    Refactor code to avoid duplications.

--- src/web_testing.py
+++ src/web_testing.py
@@ -29,6 +29,32 @@ class WebTesting():
         self.driver = webdriver.Chrome(service=service, options=options)
         self.driver.get(link)
 
+        self.xpath_latest_feed = '/html/body/div[1]/div/div[2]/section[2]/div/div/div[1]/div/section[1]/div/div/header/div/div[1]/article/div[2]/div/div[2]/span'
+        self.xpath_latest_name = '//*[@id="oC6bsXSsG"]/header/div/div[1]/article/div[2]/div/div[1]/a[1]'
+        self.xpath_latest_title = '//*[@id="oC6bsXSsG"]/div[1]/h4' 
+    
+    
+    def log_in(self, user: str, password: str) -> None:
+        """[summary]
+            Assumes that no two-factor authentication is running
+        Parameters
+        ----------
+        user : str
+            [description]
+        password : str
+            [description]
+        """
+        css_path_insedi_login = 'a.button.is-fullwidth.is-centered'
+        button_insedi_login = self.driver.find_element(By.CSS_SELECTOR, css_path_insedi_login)
+        button_insedi_login.click()
+
+        x_path_imperial_login_user = '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div[1]/div[3]/div/div/div/div[2]/div[2]/div/input[1]'
+        button_imperial_login_user = WebTesting.get_web_element(self.driver, x_path_imperial_login_user)
+        WebTesting.send_keys_and_click_enter_from_element(button_imperial_login_user, user)
+
+        x_path_imperial_login_password = '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div/div[2]/input'
+        button_imperial_login_password = WebTesting.get_web_element(self.driver, x_path_imperial_login_password)
+        WebTesting.send_keys_and_click_enter_from_element(button_imperial_login_password, password)
 
     @staticmethod
     def get_web_element(driver, x_path: str) -> WebElement:
@@ -67,42 +93,23 @@ class WebTesting():
         web_element.send_keys(keys)
         web_element.send_keys(Keys.RETURN)
 
-
-    def log_in(self, user: str, password: str) -> None:
-        """[summary]
-            Assumes that no two-factor authentication is running
-        Parameters
-        ----------
-        user : str
-            [description]
-        password : str
-            [description]
-        """
-        css_path_insedi_login = 'a.button.is-fullwidth.is-centered'
-        button_insedi_login = self.driver.find_element(By.CSS_SELECTOR, css_path_insedi_login)
-        button_insedi_login.click()
-
-        x_path_imperial_login_user = '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div[1]/div[3]/div/div/div/div[2]/div[2]/div/input[1]'
-        button_imperial_login_user = WebTesting.get_web_element(self.driver, x_path_imperial_login_user)
-        WebTesting.send_keys_and_click_enter_from_element(button_imperial_login_user, user)
-
-        x_path_imperial_login_password = '/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div/div[2]/input'
-        button_imperial_login_password = WebTesting.get_web_element(self.driver, x_path_imperial_login_password)
-        WebTesting.send_keys_and_click_enter_from_element(button_imperial_login_password, password)
-
     
     @staticmethod
-    def get_date_latest_news_feed_of_module(driver: webdriver) -> str:
+    def get_attribute_latest_news_feed_of_module(driver: webdriver, x_path: str) -> str:
+
+        last_posted_date_element = WebTesting.get_web_element(driver=driver, x_path=x_path)
+        last_posted_date_text = last_posted_date_element.text
+
+        return last_posted_date_text   
+
+
+    @staticmethod
+    def navigate_to_news_feed_of_module(driver: webdriver):
         """[summary]
 
         Parameters
         ----------
         driver : webdriver
-            [description]
-
-        Returns
-        -------
-        str
             [description]
         """
         x_path_dat_algo_module = '//*[@id="main-content"]/section[3]/div/div/div/section/section[1]/div[2]/div/div/div[8]/div[2]/a'
@@ -112,49 +119,6 @@ class WebTesting():
         x_path_news_feed = '//*[@id="navMenu"]/ul/li[2]/div/a/span'
         news_feed_element = WebTesting.get_web_element(driver=driver, x_path=x_path_news_feed)
         news_feed_element.click()
-
-        x_path_date_last_feed = '/html/body/div[1]/div/div[2]/section[2]/div/div/div[1]/div/section[1]/div/div/header/div/div[1]/article/div[2]/div/div[2]/span'
-        last_posted_date_element = WebTesting.get_web_element(driver=driver, x_path=x_path_date_last_feed)
-        last_posted_date_text = last_posted_date_element.text
-
-        return last_posted_date_text
-
-
-    @staticmethod
-    def get_name_latest_news_feed_of_module(driver: webdriver) -> str:
-        """[summary]
-
-        Returns
-        -------
-        str
-            [description]
-        """
-        x_path_name_last_feed = '//*[@id="oC6bsXSsG"]/header/div/div[1]/article/div[2]/div/div[1]/a[1]'
-        last_posted_name_element = WebTesting.get_web_element(driver=driver, x_path=x_path_name_last_feed)
-        last_posted_name_text = last_posted_name_element.text
-
-        return last_posted_name_text
-
-
-    @staticmethod
-    def get_title_lastest_news_feed_of_module(driver:webdriver) -> str:
-        """[summary]
-
-        Parameters
-        ----------
-        driver : webdriver
-            [description]
-
-        Returns
-        -------
-        str
-            [description]
-        """
-        x_path_title_last_feed = '//*[@id="oC6bsXSsG"]/div[1]/h4' 
-        last_posted_title_element = WebTesting.get_web_element(driver=driver, x_path=x_path_title_last_feed)
-        last_posted_title_text = last_posted_title_element.text
-
-        return last_posted_title_text
 
 
     def append_date_of_las_posted_feed_to_csv(self):
@@ -168,9 +132,11 @@ class WebTesting():
         session_date = datetime.today()
         session_date_str = session_date.strftime('%d %B %Y')
 
-        last_feed_date_str = WebTesting.get_date_latest_news_feed_of_module(self.driver)
-        last_feed_name_str = WebTesting.get_name_latest_news_feed_of_module(self.driver)
-        last_feed_title_str = WebTesting.get_title_lastest_news_feed_of_module(self.driver)
+        WebTesting.navigate_to_news_feed_of_module(driver=self.driver)
+
+        last_feed_date_str = WebTesting.get_attribute_latest_news_feed_of_module(driver=self.driver, x_path=self.xpath_latest_feed)
+        last_feed_name_str = WebTesting.get_attribute_latest_news_feed_of_module(driver=self.driver, x_path=self.xpath_latest_name)
+        last_feed_title_str = WebTesting.get_attribute_latest_news_feed_of_module(driver=self.driver, x_path=self.xpath_latest_title)
 
         df = pd.DataFrame({'date_selenium_session': [session_date_str], 'date_last_feed_posted': last_feed_date_str, 
         'name_last_feed_posted': last_feed_name_str, 'title_last_feed_posted': last_feed_title_str})
